edea74d4bb2ba1ab8b627bfce74d123f4e9c6ea1
safintofiqali <safintofiqali@gmail.com>
2022-06-05 17:32:29 +0300

    Adding all the files

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import birthdaysData from "./data";
 import "./App.css";
 import List from "./components/list/List.comp";
-import Pagination from "./components/pagination/Pagination.comp";
 
 const App = () => {
   const clearAllBirthdays = () => {
     setBirthdays([]);
+    setAction("Load Birthdays");
   };
 
-  const changePageNumber = (pageNumber) => {
-    setPage(pageNumber);
+  const addBirthdays = () => {
+    setBirthdays(birthdaysData);
+    setAction("Clear All");
   };
 
   const [birthdays, setBirthdays] = useState(birthdaysData);
-  const [page, setPage] = useState(1);
-  const articlePerPage = 3;
-  const indexOfLastItem = page * articlePerPage;
-  const indexOfFirstItem = indexOfLastItem - articlePerPage;
-
-  useEffect(() => {
-    let birthdaysArray = birthdaysData.slice(indexOfFirstItem, indexOfLastItem);
-    setBirthdays(birthdaysArray);
-  }, [page]);
+  const [action, setAction] = useState("Clear All");
 
   return (
     <main>
       <section className="container">
         <h3>{birthdays.length} Birthdays today</h3>
         <List birthdays={birthdays} />
-        <Pagination
-          paginate={changePageNumber}
-          articlesPerPage={articlePerPage}
-          articles={birthdaysData.length}
-        />
-        <button onClick={clearAllBirthdays}>Clear All</button>
+        <button
+          onClick={action === "Clear All" ? clearAllBirthdays : addBirthdays}
+        >
+          {action}
+        </button>
       </section>
     </main>
   );
